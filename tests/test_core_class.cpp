c8a9e9ef7f429370ff2b7fea4c369378ac37625b
Alexandre Bleuler <alexandre.bleuler@yahoo.com>
2025-12-22 16:57:02 +0100

    rewriting test with different numbers for rows and columns

--- tests/test_core_class.cpp
+++ tests/test_core_class.cpp
@@ -69,28 +69,28 @@ The beginning 1 correspond to the first '/n' added to ensure convenient display.
 
 TEST_CASE("Grid display, Cell::fill(), Cell::clear()")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
     REQUIRE(get_grid(grid)==expected_grid);
-    expected_grid[1+6*11+5]='O';
+    expected_grid[1+6*12+5]='O';
     Block block{6,5};
     grid(6,5).fill(block);
     REQUIRE(get_grid(grid)==expected_grid);
 
-    expected_grid[1+6*11+5]='.';
+    expected_grid[1+6*12+5]='.';
     grid(6,5).clear();
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::put_piece, O type")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+1*11+4]='O';
-    expected_grid[1+1*11+5]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+1*12+4]='O';
+    expected_grid[1+1*12+5]='O';
     Piece piece=grid.put_piece(PieceType::O);
     REQUIRE(get_grid(grid)==expected_grid);
 }
@@ -98,157 +98,157 @@ TEST_CASE("Grid::put_piece, O type")
 
 TEST_CASE("Grid::put_piece, I type")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
-    expected_grid[1+0*11+3]='O';
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
+    expected_grid[1+0*12+3]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
     Piece piece=grid.put_piece(PieceType::I);
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::put_piece, S type")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
-    expected_grid[1+1*11+5]='O';
-    expected_grid[1+1*11+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
+    expected_grid[1+1*12+5]='O';
+    expected_grid[1+1*12+4]='O';
     Piece piece=grid.put_piece(PieceType::S);
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::put_piece, Z type")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+1*11+5]='O';
-    expected_grid[1+1*11+6]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+1*12+5]='O';
+    expected_grid[1+1*12+6]='O';
     Piece piece=grid.put_piece(PieceType::Z);
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::put_piece, L type")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
-    expected_grid[1+1*11+4]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
+    expected_grid[1+1*12+4]='O';
     Piece piece=grid.put_piece(PieceType::L);
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::put_piece, J type")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
-    expected_grid[1+1*11+6]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
+    expected_grid[1+1*12+6]='O';
     Piece piece=grid.put_piece(PieceType::J);
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::put_piece, T type")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
-    expected_grid[1+1*11+5]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
+    expected_grid[1+1*12+5]='O';
     Piece piece=grid.put_piece(PieceType::T);
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::move_piece, O type, no edge cases")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
 
     Piece piece=grid.put_piece(PieceType::O);
 
     bool has_moved=grid.move_piece(piece, Move::down);
     REQUIRE(has_moved==true);
-    expected_grid[1+1*11+4]='O';
-    expected_grid[1+1*11+5]='O';
-    expected_grid[1+2*11+4]='O';
-    expected_grid[1+2*11+5]='O';
+    expected_grid[1+1*12+4]='O';
+    expected_grid[1+1*12+5]='O';
+    expected_grid[1+2*12+4]='O';
+    expected_grid[1+2*12+5]='O';
     REQUIRE(get_grid(grid)==expected_grid);
 
-    expected_grid=set_empty_grid(10,10);
+    expected_grid=set_empty_grid(10,11);
     has_moved=grid.move_piece(piece, Move::up);
     REQUIRE(has_moved==true);
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+1*11+4]='O';
-    expected_grid[1+1*11+5]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+1*12+4]='O';
+    expected_grid[1+1*12+5]='O';
     REQUIRE(get_grid(grid)==expected_grid);
 
-    expected_grid=set_empty_grid(10,10);
+    expected_grid=set_empty_grid(10,11);
     has_moved=grid.move_piece(piece, Move::right);
     REQUIRE(has_moved==true);
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
-    expected_grid[1+1*11+5]='O';
-    expected_grid[1+1*11+6]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
+    expected_grid[1+1*12+5]='O';
+    expected_grid[1+1*12+6]='O';
     REQUIRE(get_grid(grid)==expected_grid);
 
-    expected_grid=set_empty_grid(10,10);
+    expected_grid=set_empty_grid(10,11);
     has_moved=grid.move_piece(piece, Move::left);
     REQUIRE(has_moved==true);
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+1*11+4]='O';
-    expected_grid[1+1*11+5]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+1*12+4]='O';
+    expected_grid[1+1*12+5]='O';
     REQUIRE(get_grid(grid)==expected_grid);
 
 
-    expected_grid=set_empty_grid(10,10);
+    expected_grid=set_empty_grid(10,11);
     has_moved=grid.move_piece(piece, Move::down);
     has_moved=grid.move_piece(piece, Move::clock_rotation);
     REQUIRE(has_moved==true);
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+1*11+4]='O';
-    expected_grid[1+1*11+5]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+1*12+4]='O';
+    expected_grid[1+1*12+5]='O';
     REQUIRE(get_grid(grid)==expected_grid);
 
-    expected_grid=set_empty_grid(10,10);
+    expected_grid=set_empty_grid(10,11);
     has_moved=grid.move_piece(piece, Move::anticlock_rotation);
     REQUIRE(has_moved==true);
-    expected_grid[1+1*11+4]='O';
-    expected_grid[1+1*11+5]='O';
-    expected_grid[1+2*11+4]='O';
-    expected_grid[1+2*11+5]='O';
+    expected_grid[1+1*12+4]='O';
+    expected_grid[1+1*12+5]='O';
+    expected_grid[1+2*12+4]='O';
+    expected_grid[1+2*12+5]='O';
     REQUIRE(get_grid(grid)==expected_grid);
 }
 
 TEST_CASE("Grid::move_piece, I type, edge cases")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
-    expected_grid[1+0*11+3]='O';
-    expected_grid[1+0*11+4]='O';
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
+    expected_grid[1+0*12+3]='O';
+    expected_grid[1+0*12+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
     Piece piece=grid.put_piece(PieceType::I);
     bool has_moved;
 
-    has_moved=grid.move_piece(piece, Move::right, 4);
+    has_moved=grid.move_piece(piece, Move::right, 5);
     REQUIRE(has_moved==false);
     REQUIRE(get_grid(grid)==expected_grid);
 
@@ -264,30 +264,12 @@ TEST_CASE("Grid::move_piece, I type, edge cases")
     REQUIRE(has_moved==false);
     REQUIRE(get_grid(grid)==expected_grid);
 
-    expected_grid=set_empty_grid(10,10);
-    has_moved=grid.move_piece(piece, Move::right, 3);
-    expected_grid[1+0*11+6]='O';
-    expected_grid[1+0*11+7]='O';
-    expected_grid[1+0*11+8]='O';
-    expected_grid[1+0*11+9]='O';
-    has_moved=grid.move_piece(piece, Move::clock_rotation);
-    REQUIRE(has_moved==false);
-    REQUIRE(get_grid(grid)==expected_grid);
-
-    has_moved=grid.move_piece(piece, Move::anticlock_rotation);
-    REQUIRE(has_moved==false);
-    REQUIRE(get_grid(grid)==expected_grid);
-    has_moved=grid.move_piece(piece, Move::left, 3);
-
-    expected_grid=set_empty_grid(10,10);
-    has_moved=grid.move_piece(piece, Move::down, 1);
-    has_moved=grid.move_piece(piece, Move::anticlock_rotation);
+    expected_grid=set_empty_grid(10,11);
     has_moved=grid.move_piece(piece, Move::right, 4);
-    expected_grid[1+0*11+9]='O';
-    expected_grid[1+1*11+9]='O';
-    expected_grid[1+2*11+9]='O';
-    expected_grid[1+3*11+9]='O';
-    
+    expected_grid[1+0*12+7]='O';
+    expected_grid[1+0*12+8]='O';
+    expected_grid[1+0*12+9]='O';
+    expected_grid[1+0*12+10]='O';
     has_moved=grid.move_piece(piece, Move::clock_rotation);
     REQUIRE(has_moved==false);
     REQUIRE(get_grid(grid)==expected_grid);
@@ -296,20 +278,38 @@ TEST_CASE("Grid::move_piece, I type, edge cases")
     REQUIRE(has_moved==false);
     REQUIRE(get_grid(grid)==expected_grid);
     has_moved=grid.move_piece(piece, Move::left, 4);
+
+    expected_grid=set_empty_grid(10,11);
+    has_moved=grid.move_piece(piece, Move::down, 1);
+    has_moved=grid.move_piece(piece, Move::anticlock_rotation);
+    has_moved=grid.move_piece(piece, Move::right, 5);
+    expected_grid[1+0*12+10]='O';
+    expected_grid[1+1*12+10]='O';
+    expected_grid[1+2*12+10]='O';
+    expected_grid[1+3*12+10]='O';
+    
+    has_moved=grid.move_piece(piece, Move::clock_rotation);
+    REQUIRE(has_moved==false);
+    REQUIRE(get_grid(grid)==expected_grid);
+
+    has_moved=grid.move_piece(piece, Move::anticlock_rotation);
+    REQUIRE(has_moved==false);
+    REQUIRE(get_grid(grid)==expected_grid);
+    has_moved=grid.move_piece(piece, Move::left, 5);
     has_moved=grid.move_piece(piece, Move::clock_rotation);
     has_moved=grid.move_piece(piece, Move::up, 1);
 
-    expected_grid=set_empty_grid(10,10);
+    expected_grid=set_empty_grid(10,11);
     has_moved=grid.move_piece(piece, Move::down, 2);
-    expected_grid[1+2*11+3]='O';
-    expected_grid[1+2*11+4]='O';
-    expected_grid[1+2*11+5]='O';
-    expected_grid[1+2*11+6]='O';
+    expected_grid[1+2*12+3]='O';
+    expected_grid[1+2*12+4]='O';
+    expected_grid[1+2*12+5]='O';
+    expected_grid[1+2*12+6]='O';
     Piece piece_wall=grid.put_piece(PieceType::S);
-    expected_grid[1+0*11+5]='O';
-    expected_grid[1+0*11+6]='O';
-    expected_grid[1+1*11+5]='O';
-    expected_grid[1+1*11+4]='O';
+    expected_grid[1+0*12+5]='O';
+    expected_grid[1+0*12+6]='O';
+    expected_grid[1+1*12+5]='O';
+    expected_grid[1+1*12+4]='O';
     has_moved=grid.move_piece(piece, Move::up, 1);
     REQUIRE(has_moved==false);
     REQUIRE(get_grid(grid)==expected_grid);
@@ -317,10 +317,10 @@ TEST_CASE("Grid::move_piece, I type, edge cases")
 
 TEST_CASE("Grid::update")
 {
-    Grid grid{10,10};
-    std::string expected_grid=set_empty_grid(10,10);
+    Grid grid{10,11};
+    std::string expected_grid=set_empty_grid(10,11);
     Block block;
-    for(unsigned int j=0; j<10; ++j)
+    for(unsigned int j=0; j<11; ++j)
     {
         grid(6,j).fill(block);
         grid(7,j).fill(block);
@@ -329,7 +329,7 @@ TEST_CASE("Grid::update")
     grid(8,0).fill(block);
     grid(5,0).fill(block);
     grid.update();
-    expected_grid[1+8*11+0]='O';
-    expected_grid[1+9*11+0]='O';
+    expected_grid[1+8*12+0]='O';
+    expected_grid[1+9*12+0]='O';
     REQUIRE(get_grid(grid)==expected_grid);
 }
